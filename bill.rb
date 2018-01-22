0c6d01b167d22d33b5a461d161d80199d9fccacf
CheezItMan <chris@adadevelopersacademy.org>
2018-01-22 13:55:28 -0800

    Added initialize.

--- bill.rb
+++ bill.rb
@@ -1,1 +1,10 @@
 # bill.rb
+
+class Bill
+  def initialize(prices)
+    if prices.class != Array || prices.length < 1
+      raise ArgumentError.new("Bill requires a non-empty array")
+    end
+    @prices = prices
+  end
+end
