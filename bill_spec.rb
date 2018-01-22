96f00a74e60d4e4246d0c8a10dfe66dea89cfd6d
CheezItMan <chris@adadevelopersacademy.org>
2018-01-22 13:49:20 -0800

    Added tests for invalid arguments.

--- bill_spec.rb
+++ bill_spec.rb
@@ -4,9 +4,21 @@ require 'minitest/reporters'
 require_relative 'bill'
 
 describe "Bill" do
-  it "Can be created" do
-    bill = Bill.new
+  it "can be created" do
+    bill = Bill.new([4.75, 8.75])
     # the class of @bill should be Bill
     bill.class.must_equal Bill
+  end
+
+  it "will raise an error if created without a list of prices" do
+    proc {
+      Bill.new
+    }.must_raise ArgumentError
+    proc {
+      Bill.new([])
+    }.must_raise ArgumentError
+    proc {
+      Bill.new(1)
+    }.must_raise ArgumentError
   end
 end
