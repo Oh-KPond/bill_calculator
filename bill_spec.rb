4713d1cca794888a9b9300af19fd4071174e5b50
CheezItMan <chris@adadevelopersacademy.org>
2018-01-22 16:28:17 -0800

    refactored testing

--- bill_spec.rb
+++ bill_spec.rb
@@ -4,10 +4,14 @@ require 'minitest/reporters'
 require_relative 'bill'
 
 describe "Bill" do
+
+  before do
+    @bill = Bill.new([4.75, 8.75])
+  end
+
   it "can be created" do
-    bill = Bill.new([4.75, 8.75])
     # the class of @bill should be Bill
-    bill.class.must_equal Bill
+    @bill.class.must_equal Bill
   end
 
   it "will raise an error if created without a list of prices" do
@@ -23,24 +27,20 @@ describe "Bill" do
   end
 
   it "can calculate the proper subtotal" do
-    # Arrange
-    bill = Bill.new([4.75, 8.75])
 
     # Act
-    subtotal = bill.subtotal
+    subtotal = @bill.subtotal
 
     # Assert
     subtotal.must_equal 13.50
   end
 
   it "can calculate the proper tax assuming 8% tax" do
-    # Arrange
-    bill = Bill.new([4.75, 8.75])
 
     # Act
-    tax = bill.tax
+    tax = @bill.tax
 
     # Assert
-    tax.must_equal bill.subtotal * 0.08
+    tax.must_equal @bill.subtotal * 0.08
   end
 end
